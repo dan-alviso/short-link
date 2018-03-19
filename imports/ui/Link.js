1599ed8982a9a9ece8a7b6352b47793741af7d88
Daniel Alviso <dan.alviso@yahoo.com>
2018-03-18 18:30:39 -0700

    Set up navigation between routes

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
 
 export default class Link extends React.Component {
   render() {
-    return <p>Link component here</p>;
+    return (
+      <div>
+        <h1>Your Links</h1>
+        <button onClick={() => browserHistory.push('/')}>Logout</button>
+      </div>
+    );
   }
 }
--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class Login extends React.Component {
   render() {
-    return <p>Login component here</p>;
+    return (
+      <div>
+        <h1>Login in to Short Link</h1>
+
+        login form here
+
+        <Link to="/signup">Don't yet have an account?</Link>
+      </div>
+    );
   }
 }
